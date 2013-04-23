ed21974ae63ce348694953283f76be096d5c7ba0
Igor <fedoronchuk@gmail.com>
2013-04-23 21:47:27 +0300

    added string extension for detecting credit card brand

---
+++ lib/credit_card_validations/string.rb
@@ -0,0 +1,9 @@
+class String
+  def credit_card_brand
+     CreditCardValidations::Detector.new(self).brand
+  end  
+  
+  def valid_credit_card_brand?(*brands)
+      CreditCardValidations::Detector.new(self).valid?(*brands)
+  end  
+end  
