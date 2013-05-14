d9af47f3972773c3c822d1b73bafa2911cecd420
Igor <fedoronchuk@gmail.com>
2013-05-14 09:46:39 +0300

    support for brands: :any  syntax  for activemodel validator

--- lib/active_model/credit_card_number_validator.rb
+++ lib/active_model/credit_card_number_validator.rb
@@ -4,7 +4,7 @@ module ActiveModel
 
       def validate_each(record, attribute, value)
         brands = options.fetch(:brands) 
-        record.errors[attribute] << (options[:message] || "is not valid") unless credit_card_valid?(value, Array.wrap(brands))
+        record.errors.add(attribute) unless credit_card_valid?(value, brands == :any ? [] : Array.wrap(brands) )
       end
 
       def credit_card_valid?(number, brands = [])
