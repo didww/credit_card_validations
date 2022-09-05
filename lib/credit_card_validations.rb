065652c1060b0369b0d2e5b1d4b1db8355a38bef
Igor Fedoronchuk <igor.f@didww.com>
2022-09-05 11:47:08 +0300

    V6.0.0 (#117)
    * allow to support own brands file
    
    * bump version
    
    changes to readme

--- lib/credit_card_validations.rb
+++ lib/credit_card_validations.rb
@@ -1,4 +1,5 @@
 require 'credit_card_validations/version'
+require 'credit_card_validations/configuration'
 require 'credit_card_validations/error'
 require 'active_model'
 require 'active_support/core_ext'
@@ -14,16 +15,36 @@ module CreditCardValidations
   autoload :Factory, 'credit_card_validations/factory'
   autoload :Mmi, 'credit_card_validations/mmi'
 
+  attr_accessor :configuration
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.reset
+    @configuration = Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+    reload!
+  end
 
   def self.add_brand(key, rules, options = {})
     Detector.add_brand(key, rules, options)
   end
 
-  DATA = YAML.load_file(File.join(File.dirname(__FILE__),  'data', 'brands.yaml')) || {}
+  def self.source
+    configuration.source || File.join(File.join(File.dirname(__FILE__)), 'data', 'brands.yaml')
+  end
+
+  def self.data
+    YAML.load_file(source) || {}
+  end
 
   def self.reload!
     Detector.brands = {}
-    DATA.each do |key, data|
+    data.each do |key, data|
       add_brand(key, data.fetch(:rules), data.fetch(:options, {}))
     end
   end
